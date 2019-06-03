8521ca8cf1e49654a4428c960074a33b2197de52
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2019-06-03 16:19:29 -0500

    Separates the weak labels into separate variables

--- lib/roi_data/fast_rcnn_rel.py
+++ lib/roi_data/fast_rcnn_rel.py
@@ -585,9 +585,10 @@ def _sample_rois_triplet_yall(
         rel_neg_ends=neg_ends_rel)
 
     if cfg.MODEL.WEAK_LABELS:
-        blob['sbj_pos_labels_int32_w'] = sbj_pos_labels_w.astype(np.int32, copy=False),# weak labels
-        blob['obj_pos_labels_int32_w'] = obj_pos_labels_w.astype(np.int32, copy=False),# weak labels
-        blob['rel_pos_labels_int32_w'] = rel_pos_labels_w.astype(np.int32, copy=False),# weak labels
+        for num_w in cfg.MODEL.NUM_WEAK_LABELS:
+            blob['sbj_pos_labels_int32_w_' + num_w] = sbj_pos_labels_w[:, num_w].astype(np.int32, copy=False) # weak labels
+            blob['obj_pos_labels_int32_w_' + num_w] = obj_pos_labels_w[:, num_w].astype(np.int32, copy=False) # weak labels
+            blob['rel_pos_labels_int32_w_' + num_w] = rel_pos_labels_w[:, num_w].astype(np.int32, copy=False) # weak labels
 
     if cfg.TRAIN.ADD_LOSS_WEIGHTS:
         blob['rel_pos_weights'] = rel_pos_weights
