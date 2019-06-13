4e27960d531f9d45ed7785ae375b1b304f3ee5cf
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2019-06-13 14:15:14 -0500

    Updates the way the accuracy is calculated for weak labels

--- lib/utils/evaluator_rel.py
+++ lib/utils/evaluator_rel.py
@@ -356,7 +356,7 @@ class Evaluator():
                         self.rel_top10_cnt += 1
 
                     if cfg.MODEL.WEAK_LABELS:
-                        if (det_labels_sbj[ind, 0:1] in gt_labels_sbj_w[:, ind] and det_labels_obj[ind, 0:1] in gt_labels_obj_w[:, ind] and det_labels_rel[ind, 0:1] in gt_labels_rel_w[:, ind]) or (gt_labels_sbj[ind] in det_labels_sbj[ind, :1] and gt_labels_obj[ind] in det_labels_obj[ind, :1] and gt_labels_rel[ind] in det_labels_rel[ind, :1]):
+                        if (det_labels_sbj[ind, 0:1] in gt_labels_sbj_w[:, ind] or gt_labels_sbj[ind] in det_labels_sbj[ind, :1]) and (det_labels_obj[ind, 0:1] in gt_labels_obj_w[:, ind] or gt_labels_obj[ind] in det_labels_obj[ind, :1]) and (det_labels_rel[ind, 0:1] in gt_labels_rel_w[:, ind] or gt_labels_rel[ind] in det_labels_rel[ind, :1]):
                             self.tri_top1_w_cnt += 1
                         if det_labels_sbj[ind, 0:1] in gt_labels_sbj_w[:, ind] or gt_labels_sbj[ind] in det_labels_sbj[ind, :1]:
                             self.sbj_top1_w_cnt += 1
