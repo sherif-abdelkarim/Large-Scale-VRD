724431c8e8da712890017fc4a552cf914b3e5f49
Sherif Abdelkarim <sherif.abdelkarim91@gmail.com>
2019-10-24 14:16:56 -0500

    Adds the centroids loss function (still missing some sections)

--- lib/modeling/VGG16_rel_softmax.py
+++ lib/modeling/VGG16_rel_softmax.py
@@ -442,66 +442,184 @@ def add_softmax_losses(model, label):
         model.loss_set.extend([loss_xp_yall])
 
 
-# def add_centroids_loss(model, feat, label, batch_size):
-#     prefix = label + '_'
-#     suffix = '_' + label
-#
-#     # batch_size = feat.size(0)
-#
-#     # calculate attracting loss
-#
-#     # feat = feat.view(batch_size, -1)
-#     model.net.Reshape([feat],
-#                       ['feat_reshaped' + suffix, 'feat_old_shape' + suffix],
-#                       shape=(batch_size, -1))
-#
-#     # To check the dim of centroids and features
-#     # if feat.size(1) != self.feat_dim:
-#     #     raise ValueError("Center's dim: {0} should be equal to input feature's \
-#     #                     dim: {1}".format(self.feat_dim, feat.size(1)))
-#     # batch_size_tensor = feat.new_empty(1).fill_(batch_size if self.size_average else 1)
-#     model.net.ConstantFill(['feat_reshaped' + suffix], 'batch_size_tensor' + suffix, value=1.0)
-#
-#     # loss_attract = self.disccentroidslossfunc(feat, label, self.centroids, batch_size_tensor).squeeze()
-#
-#     loss_attract = disc_centroids_loss_func('feat_reshaped' + suffix, prefix + 'pos_labels_int32', centroids,
-#                                             'batch_size_tensor' + suffix)
-#     # calculate repelling loss
-#
-#     distmat = torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
-#               torch.pow(self.centroids, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
-#     distmat.addmm_(1, -2, feat, self.centroids.t())
-#
-#     classes = torch.arange(self.num_classes).long().cuda()
-#     labels_expand = label.unsqueeze(1).expand(batch_size, self.num_classes)
-#     mask = labels_expand.eq(classes.expand(batch_size, self.num_classes))
-#
-#     distmat_neg = distmat
-#     distmat_neg[mask] = 0.0
-#     # margin = 50.0
-#     margin = 10.0
-#     loss_repel = torch.clamp(margin - distmat_neg.sum() / (batch_size * self.num_classes), 0.0, 1e6)
-#
-#     # loss = loss_attract + 0.05 * loss_repel
-#     loss = loss_attract + 0.01 * loss_repel
-#
-#     return loss
+def add_centroids_loss(model, feat, label, batch_size):
+    prefix = label + '_'
+    suffix = '_' + label
 
-# def disc_centroids_loss_func(feature, labels, centroids, batch_size_tensor):
-#     # centroids_expand = centroids.unsqueeze(0).expand(batch_size, -1, -1)
-#     model.net.ExpandDims([centroids_blob_name],
-#                         ['centroids_expanddims' + suffix],
-#                         dims=[0])
-#
-#     model.net.Tile('centroids_expanddims' + suffix,
-#                   'centroids_expand' + suffix,
-#                   tiles=batch_size,
-#                   axis=0)
-#
-#
-#     # ctx.save_for_backward(feature, label, centroids, batch_size)
-#     # centroids_batch = centroids.index_select(0, label.long())
-#     # return (feature - centroids_batch).pow(2).sum() / 2.0 / batch_size
+    # batch_size = feat.size(0)
+
+    # calculate attracting loss
+
+    # feat = feat.view(batch_size, -1)
+    # (128, 1024)
+    model.net.Reshape([feat],
+                      ['feat_reshaped' + suffix, 'feat_old_shape' + suffix],
+                      shape=(batch_size, -1))
+
+    # To check the dim of centroids and features
+    # if feat.size(1) != self.feat_dim:
+    #     raise ValueError("Center's dim: {0} should be equal to input feature's \
+    #                     dim: {1}".format(self.feat_dim, feat.size(1)))
+    # batch_size_tensor = feat.new_empty(1).fill_(batch_size if self.size_average else 1)
+    model.net.ConstantFill(['feat_reshaped' + suffix], 'batch_size_tensor' + suffix, value=1.0)
+
+    # loss_attract = self.disccentroidslossfunc(feat, label, self.centroids, batch_size_tensor).squeeze()
+
+    loss_attract = disc_centroids_loss_func('feat_reshaped' + suffix, prefix + 'pos_labels_int32', centroids,
+                                            'batch_size_tensor' + suffix, label) #TODO: Check the backward function in original implementation
+    # calculate repelling loss
+
+    # distmat = torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+    #           torch.pow(self.centroids, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+
+    # torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes)
+    #torch.pow(feat, 2)
+    model.net.Sqr(['feat_reshaped' + suffix], ['feat_squared' + suffix])
+
+    #.sum(dim=1, keepdim=True)
+    split = tuple([1 for i in range(cfg.OUTPUT_EMBEDDING_DIM)])
+    tensors_list_names = ['sum_' + str(i) + suffix for i in range(cfg.OUTPUT_EMBEDDING_DIM)]
+    tensors_list = model.net.Split('feat_squared' + suffix, tensors_list_names, axis=1, split=split)
+    model.net.Sum(tensors_list, 'feat_squared_sum' + suffix) #(128, 1)
+
+    #.expand(batch_size, self.num_classes)
+    model.net.Tile('feat_squared_sum' + suffix,
+                  'feat_squared_sum_expand' + suffix,
+                  tiles=num_classes,
+                  axis=1) #(128, 1703)
+
+    # torch.pow(self.centroids, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+    # torch.pow(self.centroids, 2)
+    # (1703, 1024)
+    model.net.Sqr(['centroids' + suffix], ['centroids_squared' + suffix])
+
+    #.sum(dim=1, keepdim=True)
+    tensors_list_names = ['centroids_sum_' + str(i) + suffix for i in range(cfg.OUTPUT_EMBEDDING_DIM)]
+    tensors_list = model.net.Split('centroids_squared' + suffix, tensors_list_names, axis=1, split=split)
+    model.net.Sum(tensors_list, 'centroids_squared_sum' + suffix) #(1703, 1)
+
+
+    model.Transpose(['centroids_squared_sum' + suffix], ['centroids_squared_sum_T' + suffix]) #(1, 1703)
+    model.net.Tile(['centroids_squared_sum_T' + suffix, 'batch_size' + suffix],
+                   'centroids_squared_sum_expand_T' + suffix,
+                   axis=0) # (128, 1703)
+
+    # distmat = torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+    #           torch.pow(self.centroids, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+
+    model.net.Sum(['feat_squared_sum_expand' + suffix, 'centroids_squared_sum_expand_T' + suffix], 'distmat' + suffix) # (128, 1703)
+
+
+    # feat: (128, 1024)
+    # centroid: (1703, 1024)
+    # centroid.t(): (1024, 1703)
+    # feat * centroid.t(): (128, 1703)
+    # distmat.addmm_(1, -2, feat, self.centroids.t())
+    model.net.MatMul([feat, 'centroids' + suffix], 'feat_dot_centroids' + suffix, trans_b=1) # (128, 1703)
+    model.net.Mul(['feat_dot_centroids' + suffix, 'neg_two_blob'], 'neg_2_feat_dot_centroids' + suffix, broadcast=1)
+    model.net.Sum(['distmat' + suffix, 'neg_2_feat_dot_centroids' + suffix], 'distmat_plus_neg_2feat_dot_centroids' + suffix) # (128, 1703)
+
+    # classes = torch.arange(self.num_classes).long().cuda()
+    #TODO
+    # should produce  'classes' + suffix
+
+    # labels_expand = label.unsqueeze(1).expand(batch_size, self.num_classes)
+    #label.unsqueeze(1)
+    model.net.ExpandDims([prefix + 'pos_labels_int32'],
+                        ['labels_expand' + suffix],
+                        dims=[1])
+    #.expand(batch_size, self.num_classes)
+    model.net.Tile('labels_expand' + suffix,
+                  'labels_expand_tile' + suffix,
+                  tiles=num_classes,
+                  axis=1) #(128, 1703)
+
+    model.net.ConstantFill(['labels_expand_tile' + suffix], 'zero_blob_mask' + suffix, value=0.0)
+
+    # classes.expand(batch_size, self.num_classes)
+    model.net.ExpandDims(['classes' + suffix],
+                        ['classes_expand' + suffix],
+                        dims=[0]) # (1, 1703)
+    model.net.Tile(['classes_expand' + suffix, 'batch_size' + suffix],
+                   'classes_expand_tile' + suffix,
+                   axis=0) # (128, 1703)
+
+    # mask = labels_expand.eq(classes.expand(batch_size, self.num_classes))
+    model.net.EQ(['labels_expand_tile' + suffix, 'classes_expand_tile' + suffix], 'mask' + suffix) # TODO: Make sure EQ func produces boolean
+
+
+    # distmat_neg = distmat
+
+    # distmat_neg[mask] = 0.0 # TODO: verify that model.net.where() works as understood
+    model.net.Where(['mask' + suffix, 'distmat_plus_neg_2feat_dot_centroids' + suffix, 'zero_blob_mask' + suffix], 'distmat_neg' + suffix) # (128, 1703)
+    # margin = 50.0
+    margin = 10.0
+    model.net.ConstantFill(['labels_expand_tile' + suffix], 'margin_blob' + suffix, value=margin)
+
+    #              <------------------------------Input---------------------------------->,  min, max
+    # loss_repel = torch.clamp(margin - distmat_neg.sum() / (batch_size * self.num_classes), 0.0, 1e6)
+    # distmat_neg.sum() / (batch_size * self.num_classes
+    model.net.ReduceBackMean('distmat_neg' + suffix, 'distmat_neg_mean' + suffix)
+
+    # margin - distmat_neg.sum() / (batch_size * self.num_classes
+    model.net.Sub(['margin_blob' + suffix, 'distmat_neg_mean' + suffix], 'margin_minus_distmat_neg' + suffix)
+
+    # torch.clamp()
+    model.net.Clip('margin_minus_distmat_neg' + suffix, 'loss_repel' + suffix, min=0.0, max=1e6)
+    ## loss = loss_attract + 0.05 * loss_repel
+
+    # loss = loss_attract + 0.01 * loss_repel
+    # 0.01 * loss_repel
+    model.net.Scale('loss_repel' + suffix, 'loss_repel_scaled' + suffix, scale=0.01)
+
+    # loss_attract + 0.01 * loss_repel
+    loss_large_margin = model.net.Sum([loss_attract, 'loss_repel_scaled' + suffix], 'loss_large_margin' + suffix)
+
+    return loss_large_margin
+
+def disc_centroids_loss_func(feature, labels, centroids, batch_size_tensor, label):
+    prefix = label + '_'
+    suffix = '_' + label
+
+    # centroids_expand = centroids.unsqueeze(0).expand(batch_size, -1, -1)
+    model.net.ExpandDims([centroids_blob_name],
+                        ['centroids_expanddims' + suffix],
+                        dims=[0])
+
+    model.net.Tile('centroids_expanddims' + suffix,
+                  'centroids_expand' + suffix,
+                  tiles=batch_size,
+                  axis=0)
+
+
+    # ctx.save_for_backward(feature, label, centroids, batch_size)
+    # centroids_batch = centroids.index_select(0, label.long())
+    # centroids: (1703, 1024)
+    # centroids.index_select: (128, 1024)
+    # feature: (128, 1024)
+    model.net.BatchGather(['centroids' + suffix, prefix + 'pos_labels_int32'], 'centroids_batch' + suffix)
+
+    # return (feature - centroids_batch).pow(2).sum() / 2.0 / batch_size
+    # (feature - centroids_batch)
+    # out shape: (128, 1024)
+    model.net.Sub([feature, 'centroids_batch' + suffix], 'feature_minus_centroids' + suffix)
+
+    # (feature - centroids_batch).pow(2)
+    # out shape: (128, 1024)
+    model.net.Sqr('feature_minus_centroids' + suffix, 'feature_minus_centroids_squared' + suffix)
+
+    # (feature - centroids_batch).pow(2).sum()
+    # out shape: (1,)
+    model.net.ReduceBackSum(['feature_minus_centroids_squared' + suffix],
+                            'feature_minus_centroids_squared_sum' + suffix,
+                            num_reduce_dims=2)
+
+    # (feature - centroids_batch).pow(2).sum() / 2.0
+    model.net.Scale('feature_minus_centroids_squared_sum' + suffix,
+                    'feature_minus_centroids_squared_sum_scaled' + suffix,
+                    scale=0.5)
+    loss_attract = model.net.Div(['feature_minus_centroids_squared_sum_scaled' + suffix, batch_size_tensor], 'loss_attract' + suffix)
+    return loss_attract
 
 
 def add_labels_and_scores_topk(model, label):
